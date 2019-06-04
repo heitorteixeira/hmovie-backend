82e874b92f3127d1311702262caa84d250b7e5aa
Heitor Teixeira <heitor.teixeira@hotmail.com>
2019-06-04 17:52:36 -0300

    first commit

---
+++ src/main/java/br/com/hmovie/HmovieBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.hmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmovieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmovieBackendApplication.class, args);
+	}
+
+}
